dd542ed554e3a5e91a03a341e821d2a25c4ea39d
abhijeet-trojan <trojanabhijeet@gmail.com>
2023-08-13 00:37:07 +0530

    updated

---
+++ build/precache-manifest.e5377a74d21fc5f0d43be5ef3abb2baa.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "79ee4dceff38c95d2615",
+    "url": "/kreate-app/static/css/main.5939a28f.chunk.css"
+  },
+  {
+    "revision": "79ee4dceff38c95d2615",
+    "url": "/kreate-app/static/js/main.79ee4dce.chunk.js"
+  },
+  {
+    "revision": "fd71e02662fe4b63ccc5",
+    "url": "/kreate-app/static/css/1.24a494cd.chunk.css"
+  },
+  {
+    "revision": "fd71e02662fe4b63ccc5",
+    "url": "/kreate-app/static/js/1.fd71e026.chunk.js"
+  },
+  {
+    "revision": "6ae23f6fc41a0090ff6b",
+    "url": "/kreate-app/static/js/runtime~main.6ae23f6f.js"
+  },
+  {
+    "revision": "795c0c78aa7ccc1022536689455fc32b",
+    "url": "/kreate-app/static/media/team-02.795c0c78.png"
+  },
+  {
+    "revision": "fd17e73262185e6aa807a439a14df70f",
+    "url": "/kreate-app/static/media/hero-img.fd17e732.png"
+  },
+  {
+    "revision": "48bc9c98a2626b88fd77f4d8022b480c",
+    "url": "/kreate-app/static/media/light-hero-bg.48bc9c98.jpg"
+  },
+  {
+    "revision": "f9e7cd439d5c58fb0706699ea07942fa",
+    "url": "/kreate-app/static/media/about-us.f9e7cd43.jpg"
+  },
+  {
+    "revision": "cd6f4b5a80daf962d540051dca994d5f",
+    "url": "/kreate-app/static/media/team-01.cd6f4b5a.png"
+  },
+  {
+    "revision": "2d41c4e9e01d6e140c45c16f7901d31c",
+    "url": "/kreate-app/static/media/team-03.2d41c4e9.png"
+  },
+  {
+    "revision": "643819a15ae4f5c2446806687f855946",
+    "url": "/kreate-app/static/media/video.643819a1.png"
+  },
+  {
+    "revision": "679cfab2a5f3f5ef7201c04a22c73c47",
+    "url": "/kreate-app/static/media/article.679cfab2.png"
+  },
+  {
+    "revision": "a97fb704d71be759a543c20302917259",
+    "url": "/kreate-app/static/media/case-study.a97fb704.png"
+  },
+  {
+    "revision": "0b074da0ba444449585f1516f6f74587",
+    "url": "/kreate-app/static/media/ava-1.0b074da0.jpg"
+  },
+  {
+    "revision": "32a3b1b08f15e0306b83317904de0d54",
+    "url": "/kreate-app/static/media/ava-2.32a3b1b0.jpg"
+  },
+  {
+    "revision": "10c69124027190f1a753dbb7de4b9da4",
+    "url": "/kreate-app/static/media/ava-3.10c69124.jpg"
+  },
+  {
+    "revision": "a36fc087e9d4a3bdf4a2577e764f05c8",
+    "url": "/kreate-app/static/media/remixicon.a36fc087.woff2"
+  },
+  {
+    "revision": "e1fbee88e6ed60a7bb903560914113bd",
+    "url": "/kreate-app/static/media/remixicon.e1fbee88.woff"
+  },
+  {
+    "revision": "acbf74d9482b525f4883e92b941852d0",
+    "url": "/kreate-app/static/media/remixicon.acbf74d9.eot"
+  },
+  {
+    "revision": "b67fa6d2619f443ab2fe260301202a96",
+    "url": "/kreate-app/static/media/remixicon.b67fa6d2.ttf"
+  },
+  {
+    "revision": "845a9881874fa9a82261bebea87945c8",
+    "url": "/kreate-app/static/media/remixicon.845a9881.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/kreate-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/kreate-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/kreate-app/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/kreate-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "7ce855173c600a869609000cc2e57f8e",
+    "url": "/kreate-app/index.html"
+  }
+];
